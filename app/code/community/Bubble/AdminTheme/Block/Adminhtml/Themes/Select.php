3e476eb6214c6e2c3501d5465b9e1181a27db857
Johann Reinke <johann.reinke@gmail.com>
2014-11-07 11:15:24 +0100

    Tag v1.1.3

--- app/code/community/Bubble/AdminTheme/Block/Adminhtml/Themes/Select.php
+++ app/code/community/Bubble/AdminTheme/Block/Adminhtml/Themes/Select.php
@@ -2,7 +2,7 @@
 /**
  * @category    Bubble
  * @package     Bubble_AdminTheme
- * @version     1.1.2
+ * @version     1.1.3
  * @copyright   Copyright (c) 2014 BubbleShop (https://www.bubbleshop.net)
  */
 class Bubble_AdminTheme_Block_Adminhtml_Themes_Select extends Mage_Adminhtml_Block_Html_Select
