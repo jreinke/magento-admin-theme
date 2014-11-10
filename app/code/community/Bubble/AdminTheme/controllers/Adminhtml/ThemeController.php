ea1c59abbdc20a2a9887bf4c3fd92ce61b28c390
Johann Reinke <johann.reinke@gmail.com>
2014-11-10 17:47:20 +0100

    Tag v1.3.0

--- app/code/community/Bubble/AdminTheme/controllers/Adminhtml/ThemeController.php
+++ app/code/community/Bubble/AdminTheme/controllers/Adminhtml/ThemeController.php
@@ -2,7 +2,7 @@
 /**
  * @category    Bubble
  * @package     Bubble_AdminTheme
- * @version     1.2.0
+ * @version     1.3.0
  * @copyright   Copyright (c) 2014 BubbleShop (https://www.bubbleshop.net)
  */
 class Bubble_AdminTheme_Adminhtml_ThemeController extends Mage_Adminhtml_Controller_Action
